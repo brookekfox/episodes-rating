665ac01903644e662f501f821c5857fb100c37fd
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-08-04 11:12:48 -0700

    remove console logs

--- src/app/series.component.ts
+++ src/app/series.component.ts
@@ -21,18 +21,14 @@ export class SeriesComponent {
   searchForEpisodes(title?: string) {
     if (!title) {
       let inputField = document.getElementsByClassName('input-box')[0] as HTMLInputElement;
-      console.log(inputField)
       title = inputField.value;
     }
     imdb.search({ title: title }, { apiKey: API_KEY }).then(data => {
     var showId:string = data.results[0].type === 'series' ? data.results[0].imdbid : data.results[1].imdbid;
     imdb.getById(showId, {apiKey: API_KEY}).then(show => {
       this.series = show;
-      console.log(this.series);
       show.episodes().then(episodes => {
         this.episodes = episodes.sort((ep1, ep2) => { return parseFloat(ep2.rating) - parseFloat(ep1.rating) });
-        // this.series._episodes = episodes.sort((ep1, ep2) => { return parseFloat(ep2.rating) - parseFloat(ep1.rating) });
-        console.log(this.series.poster);
         this.zone.run(() => {});
       });
     });

b4964f559a44918abfcb17e027642ac39247f0ce
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-08-04 11:47:24 -0700

    added range, median, and average

--- src/app/series.component.ts
+++ src/app/series.component.ts
@@ -17,6 +17,10 @@ export class SeriesComponent {
 
   series:any;
   episodes:any;
+  episodesRatings:number[] = [];
+  range:string;
+  average:string;
+  median:number;
 
   searchForEpisodes(title?: string) {
     if (!title) {
@@ -29,9 +33,27 @@ export class SeriesComponent {
       this.series = show;
       show.episodes().then(episodes => {
         this.episodes = episodes.sort((ep1, ep2) => { return parseFloat(ep2.rating) - parseFloat(ep1.rating) });
+        let sum:number = 0;
+        for (let ep of episodes) {
+          let rating:number = parseFloat(ep.rating);
+          this.episodesRatings.push(rating);
+          sum += rating;
+        }
+        this.range = (this.episodesRatings[0] - this.episodesRatings[this.episodesRatings.length-1]).toFixed(1);
+        this.average = (sum / this.episodesRatings.length).toFixed(2);
+        this.median = this.medianOfArray(this.episodesRatings);
         this.zone.run(() => {});
       });
     });
   });
+  }
+  
+  medianOfArray(array:number[]) {
+    let half:number = Math.floor(array.length / 2);
+    if (array.length % 2) {
+      return array[half];
+    } else {
+      return (array[half-1] + array[half]) / 2.0;
+    }
   }
 }
